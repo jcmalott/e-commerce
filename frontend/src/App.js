ca0d3928d8ede3496ae84fe5fe27fb4086b5a67b
Jonathan Malott <jonathan.malott@claritymis.com>
2022-03-21 13:09:20 -0700

    add products and images, then display them

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import data from "./database/data";
 
 const App = () => {
   return (
@@ -6,7 +7,27 @@ const App = () => {
       <header>
         <a href="/">E-Commerce</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 };
